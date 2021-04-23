b562f27e48f0c108965395529475abbd24b64a8e
nabeel siddiqui 14682 <68825270+nabeel-siddiqui-14682@users.noreply.github.com>
2021-04-24 01:55:51 +0500

    record api integrated.
    record api not working in postman as well as in application.

--- client/DocSahab_RN_Code/src/screens/Dashboard/MedicalRecord.js
+++ client/DocSahab_RN_Code/src/screens/Dashboard/MedicalRecord.js
@@ -6,18 +6,66 @@ import NavigationBtn from '../../components/navigationBtn';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { SearchBar } from 'react-native-elements';
 import DashboardScreen from '../Dashboard/dashboard';
+import { globalStyles } from '../../styles/globalStyles';
+import DocumentPicker from 'react-native-document-picker';
 
 class MedicalRecord extends React.Component {
     state = {
         search: '',
+        file: null
     };
 
     updateSearch = (search) => {
         this.setState({ search });
     };
 
+    selectRecord = async () => {
+        try {
+            const res = await DocumentPicker.pick({
+                type: [DocumentPicker.types.allFiles]
+            });
+            console.log('res: ' + JSON.stringify(res));
+            this.setState(res);
+        }
+        catch (err) {
+            this.setState(null);
+            if (DocumentPicker.isCancel(err)) {
+                alert('Selection Canceled');
+            }
+            else {
+                alert('Error: ' + JSON.stringify(err));
+            }
+        }
+    };
+
+    uploadRecord = async (file) => {
+        if (file != null) {
+            const recordToUpload = file;
+            const data = new FormData();
+            data.append('name', 'Record Upload');
+            data.append('file', recordToUpload);
+            
+            let res = await fetch(
+                'http://192.168.0.105:5000/api/upload-report', // upload record api url
+                {
+                    method: 'post',
+                    body: data,
+                    headers: {
+                        'Content-Type': 'multipart/form-data',
+                    },
+                }
+            );
+            let resJSON = await res.json();
+            if (resJSON.status == 1) {
+                alert('Upload Successful');
+            }
+        } else {
+            alert('Please select record first');
+        }
+    };
+
     render() {
-        const { search } = this.state;
+        const { search, file } = this.state;
 
         return (
             // root container
@@ -158,6 +206,97 @@ class MedicalRecord extends React.Component {
                                             <Text style={styles.SwapableViewsTitle}>Upload</Text>
                                         </View>
 
+                                        <View style={{
+                                            flex: 1,
+                                            alignItems: 'center'
+                                        }}>
+                                            {/* to show the selected file */}
+                                            <View style={{
+                                                height: 250,
+                                                width: 350,
+                                                marginTop: '5%',
+                                                marginBottom: '10%',
+                                                // justifyContent: 'center',
+                                            }}>
+                                                <Text style={{ textAlign: 'center' }}>To show the files</Text>
+                                                {/* to show the select record */}
+                                                { file != null ? (
+                                                    <Text style={{
+                                                        // textAlign: 'center',
+                                                        // fontSize: '24'
+                                                    }}>
+                                                        Record Name: { file ? file : '' }
+                                                        { console.log('hello hello: ', file) }
+                                                    </Text>
+                                                ): null }
+                                                
+                                            </View>
+
+                                            {/* select records btn */}
+                                            <View style={{
+                                                flex: 0.2,
+                                                width: 300,
+                                                justifyContent: 'center',
+                                                alignItems: 'center',
+                                                alignContent: 'center',
+                                                marginBottom: '5%'
+                                            }}>
+                                                <TouchableOpacity 
+                                                    style={ styles.UploadButton }
+                                                    onPress={ () => { this.selectRecord() } }
+                                                >
+                                                    <Text style={{
+                                                        color: 'white',
+                                                        fontSize: 18,
+                                                        marginRight: '3%',
+                                                    }}>
+                                                        Select Record
+                                                    </Text>
+                                                    <View style={{
+                                                        justifyContent: 'center',
+                                                    }}>
+                                                        <Icon
+                                                            name={"file"}
+                                                            size={24}
+                                                            color="white"
+                                                        />
+                                                    </View>
+                                                </TouchableOpacity>
+                                            </View>
+                                            
+                                            {/* upload records btn */}
+                                            <View style={{
+                                                flex: 0.2,
+                                                width: 300,
+                                                justifyContent: 'center',
+                                                alignItems: 'center',
+                                                alignContent: 'center',
+                                                marginBottom: '5%'
+                                            }}>
+                                                <TouchableOpacity 
+                                                    style={ styles.UploadButton }
+                                                    onPress = { (file) => { this.uploadRecord(file) } }
+                                                >
+                                                    <Text style={{
+                                                        color: 'white',
+                                                        fontSize: 18,
+                                                        marginRight: '3%',
+                                                    }}>
+                                                        Upload Report
+                                                    </Text>
+                                                    <View style={{
+                                                        justifyContent: 'center',
+                                                    }}>
+                                                        <Icon
+                                                            name={"cloud-upload"}
+                                                            size={24}
+                                                            color="white"
+                                                        />
+                                                    </View>
+                                                </TouchableOpacity>
+                                            </View>
+                                        </View>
+
                                     </ScrollView>
                                 </View>
 
@@ -221,6 +360,17 @@ const styles = StyleSheet.create({
         padding: 20,
         width: 430,
         marginLeft: 20,
+    },
+    UploadButton: {
+        flexDirection: 'row',
+        width:"100%",
+        color: 'white',
+        backgroundColor: '#2A2AC0',
+        borderRadius:25,
+        height:50,
+        alignItems:"center",
+        justifyContent:"center",
+        alignSelf: 'center',
     },
 });
 
