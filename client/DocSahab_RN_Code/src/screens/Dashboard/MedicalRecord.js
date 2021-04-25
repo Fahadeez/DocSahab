689bca0e2f5316d5d4fb51bbf974e6673013b89d
Fahadeez <fahadeez.paki@gmail.com>
2021-04-25 16:49:49 +0500

    Upload records done

--- client/DocSahab_RN_Code/src/screens/Dashboard/MedicalRecord.js
+++ client/DocSahab_RN_Code/src/screens/Dashboard/MedicalRecord.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView, ToastAndroid } from 'react-native';
 import NavigationBtn from '../../components/navigationBtn';
 // import { globalStyles } from '../../styles/globalStyles';
 // import Signin from '../Auth/login';
@@ -8,11 +8,16 @@ import { SearchBar } from 'react-native-elements';
 import DashboardScreen from '../Dashboard/dashboard';
 import { globalStyles } from '../../styles/globalStyles';
 import DocumentPicker from 'react-native-document-picker';
-
+import axios from 'axios';
+const config = {
+    headers: {
+        'content-type': 'multipart/form-data'
+    }
+}
 class MedicalRecord extends React.Component {
     state = {
         search: '',
-        file: null
+        file: null,
     };
 
     updateSearch = (search) => {
@@ -25,10 +30,9 @@ class MedicalRecord extends React.Component {
                 type: [DocumentPicker.types.allFiles]
             });
             console.log('res: ' + JSON.stringify(res));
-            this.setState(res);
+            this.setState({ file: res });
         }
         catch (err) {
-            this.setState(null);
             if (DocumentPicker.isCancel(err)) {
                 alert('Selection Canceled');
             }
@@ -38,29 +42,27 @@ class MedicalRecord extends React.Component {
         }
     };
 
-    uploadRecord = async (file) => {
+    uploadRecord = async () => {
+       const { file } = this.state;
+        console.log("file",file)
         if (file != null) {
             const recordToUpload = file;
             const data = new FormData();
-            data.append('name', 'Record Upload');
             data.append('file', recordToUpload);
-            
-            let res = await fetch(
-                'http://192.168.0.105:5000/api/upload-report', // upload record api url
-                {
-                    method: 'post',
-                    body: data,
+
+            let res = await axios.post(
+                'http://192.168.10.8:5000/api/upload-report',data,{
                     headers: {
-                        'Content-Type': 'multipart/form-data',
-                    },
-                }
+                    'Content-Type': 'multipart/form-data',
+                },
+            }
             );
-            let resJSON = await res.json();
-            if (resJSON.status == 1) {
-                alert('Upload Successful');
+            if (res.status === 200) {
+                ToastAndroid.show("Upload successfull",ToastAndroid.LONG)
             }
         } else {
-            alert('Please select record first');
+            ToastAndroid.show("Select a file first",ToastAndroid.LONG)
+
         }
     };
 
@@ -78,18 +80,18 @@ class MedicalRecord extends React.Component {
                     showsHorizontalScrollIndicator={false}
                 >
                     {/* sub root container */}
-                    <View style={ styles.containerForMedicalRecord }>
-                        <NavigationBtn screenName={DashboardScreen} styling={ styles.headerNavigation }/>
+                    <View style={styles.containerForMedicalRecord}>
+                        <NavigationBtn screenName={DashboardScreen} styling={styles.headerNavigation} />
 
-                        <View style={{ 
+                        <View style={{
                             // marginBottom: '5%'
                         }}>
-                            <Text style={ styles.headerTxt }>
+                            <Text style={styles.headerTxt}>
                                 Medical Records
                             </Text>
                         </View>
 
-                        <View style={{ marginBottom: '10%' }}>
+                        {/* <View style={{ marginBottom: '10%' }}>
                             <SearchBar
                                 inputContainerStyle={{ backgroundColor: 'white' }}
                                 containerStyle={{
@@ -105,19 +107,19 @@ class MedicalRecord extends React.Component {
                                 onChangeText={this.updateSearch}
                                 value={search}
                             />
-                        </View>
+                        </View> */}
 
                         {/* records details tab navigation */}
-                        <View style={ styles.RecordsInfoTabNavigation }>
+                        <View style={styles.RecordsInfoTabNavigation}>
                             <ScrollView
                                 horizontal={true}
                                 showsVerticalScrollIndicator={false}
                                 showsHorizontalScrollIndicator={false}>
-                                
+
                                 {/* View Screen */}
 
                                 <View
-                                    style={ styles.ViewScreen }
+                                    style={styles.ViewScreen}
                                 >
                                     <ScrollView
                                         showsVerticalScrollIndicator={false}
@@ -132,11 +134,11 @@ class MedicalRecord extends React.Component {
                                             flexDirection: 'row',
                                             alignItems: 'center',
                                         }}>
-                                            <View style={{ 
+                                            <View style={{
                                                 flexDirection: 'row',
                                                 width: '70%',
                                             }}>
-                                                <View style={{ 
+                                                <View style={{
                                                     flexDirection: 'column',
                                                 }}>
                                                     <View style={{ marginBottom: '0.3%' }}>
@@ -152,22 +154,22 @@ class MedicalRecord extends React.Component {
                                                 </View>
                                             </View>
 
-                                            <View style={{ 
+                                            <View style={{
                                                 flexDirection: 'row',
                                                 justifyContent: 'flex-end',
                                                 width: '30%'
-                                             }}>
+                                            }}>
                                                 <View style={{ marginEnd: '3%' }}>
                                                     <Icon
                                                         name={"arrow-circle-down"}
-                                                        size={20} 
+                                                        size={20}
                                                         color="#2A2AC0"
                                                     />
                                                 </View>
                                                 <View>
                                                     <TouchableOpacity>
                                                         <View>
-                                                            <Text style={{ 
+                                                            <Text style={{
                                                                 color: '#2A2AC0',
                                                                 fontSize: 16,
                                                                 fontWeight: '500'
@@ -178,30 +180,30 @@ class MedicalRecord extends React.Component {
                                                     </TouchableOpacity>
                                                 </View>
                                             </View>
-                                        
+
                                         </View>
 
                                         <View
-                                                style={{
-                                                    borderBottomColor: 'lightgrey',
-                                                    borderBottomWidth: 1,
-                                                    marginTop: 15,
-                                                    marginBottom: 15
-                                                }}
+                                            style={{
+                                                borderBottomColor: 'lightgrey',
+                                                borderBottomWidth: 1,
+                                                marginTop: 15,
+                                                marginBottom: 15
+                                            }}
                                         />
-                                        
+
 
                                     </ScrollView>
                                 </View>
 
                                 {/* Upload Screen */}
                                 <View
-                                    style={ styles.UploadScreen }
+                                    style={styles.UploadScreen}
                                 >
                                     <ScrollView
                                         showsVerticalScrollIndicator={false}
                                         showsHorizontalScrollIndicator={false}>
-                                        
+
                                         <View style={styles.SwapableViews}>
                                             <Text style={styles.SwapableViewsTitle}>Upload</Text>
                                         </View>
@@ -220,16 +222,16 @@ class MedicalRecord extends React.Component {
                                             }}>
                                                 <Text style={{ textAlign: 'center' }}>To show the files</Text>
                                                 {/* to show the select record */}
-                                                { file != null ? (
+                                                {file != null ? (
                                                     <Text style={{
                                                         // textAlign: 'center',
                                                         // fontSize: '24'
                                                     }}>
-                                                        Record Name: { file ? file : '' }
-                                                        { console.log('hello hello: ', file) }
+                                                        Record Name: { file ? file.name : ''}
+                                                        { console.log('hello hello: ', file)}
                                                     </Text>
-                                                ): null }
-                                                
+                                                ) : null}
+
                                             </View>
 
                                             {/* select records btn */}
@@ -241,9 +243,9 @@ class MedicalRecord extends React.Component {
                                                 alignContent: 'center',
                                                 marginBottom: '5%'
                                             }}>
-                                                <TouchableOpacity 
-                                                    style={ styles.UploadButton }
-                                                    onPress={ () => { this.selectRecord() } }
+                                                <TouchableOpacity
+                                                    style={styles.UploadButton}
+                                                    onPress={() => { this.selectRecord() }}
                                                 >
                                                     <Text style={{
                                                         color: 'white',
@@ -263,7 +265,7 @@ class MedicalRecord extends React.Component {
                                                     </View>
                                                 </TouchableOpacity>
                                             </View>
-                                            
+
                                             {/* upload records btn */}
                                             <View style={{
                                                 flex: 0.2,
@@ -273,9 +275,9 @@ class MedicalRecord extends React.Component {
                                                 alignContent: 'center',
                                                 marginBottom: '5%'
                                             }}>
-                                                <TouchableOpacity 
-                                                    style={ styles.UploadButton }
-                                                    onPress = { (file) => { this.uploadRecord(file) } }
+                                                <TouchableOpacity
+                                                    style={styles.UploadButton}
+                                                    onPress={this.uploadRecord}
                                                 >
                                                     <Text style={{
                                                         color: 'white',
@@ -336,7 +338,7 @@ const styles = StyleSheet.create({
         height: 550
     },
     ViewScreen: {
-        flexDirection: "column", 
+        flexDirection: "column",
         backgroundColor: "white",
         borderRadius: 15,
         padding: 20,
@@ -353,7 +355,7 @@ const styles = StyleSheet.create({
     },
     UploadScreen: {
         flex: 1,
-        flexDirection: "column", 
+        flexDirection: "column",
         backgroundColor: "white",
         borderRadius: 15,
         alignItems: 'center',
@@ -363,13 +365,13 @@ const styles = StyleSheet.create({
     },
     UploadButton: {
         flexDirection: 'row',
-        width:"100%",
+        width: "100%",
         color: 'white',
         backgroundColor: '#2A2AC0',
-        borderRadius:25,
-        height:50,
-        alignItems:"center",
-        justifyContent:"center",
+        borderRadius: 25,
+        height: 50,
+        alignItems: "center",
+        justifyContent: "center",
         alignSelf: 'center',
     },
 });
